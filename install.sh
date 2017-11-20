5a3c2429a23a7fa30c0c375d79f19a51fe6f0876
Trevor Brooks <tbrooks@mtech.edu>
2017-11-20 14:24:22 -0700

    Add indents to try and fix nested heredoc

--- install.sh
+++ install.sh
@@ -29,50 +29,50 @@ useradd -m -g wheel -s /usr/bin/zsh tbrooks
 echo " %wheel  ALL=(ALL) ALL" >> /etc/sudoers
 
 sudo -u tbrooks bash << EOF
-cd ~
-mkdir tmp
-cd tmp
-gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-curl -o PKGBUILD "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower"
-makepkg -i PKGBUILD
-curl -o PKGBUILD "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur"
-makepkg -i PKGBUILD
-cd ..
-rm -r tmp
+    cd ~
+    mkdir tmp
+    cd tmp
+    gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+    curl -o PKGBUILD "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower"
+    makepkg -i PKGBUILD
+    curl -o PKGBUILD "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur"
+    makepkg -i PKGBUILD
+    cd ..
+    rm -r tmp
 
-pacaur -S polybar
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mkdir .config/rofi
+    pacaur -S polybar
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    mkdir .config/rofi
 
-mkdir projects
-cd projects
-git clone https://github.com/trevorlbrooks/dotfiles
-cd
-ln -s /home/tbrooks/projects/dotfiles/.vimrc .vimrc
-mkdir .vim
-mkdir .vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir .vim/colors
-curl -o .vim/colors/256-grayvim.vim http://www.vim.org/scripts/download_script.php?src_id=12849
-pacaur -S texworks
+    mkdir projects
+    cd projects
+    git clone https://github.com/trevorlbrooks/dotfiles
+    cd
+    ln -s /home/tbrooks/projects/dotfiles/.vimrc .vimrc
+    mkdir .vim
+    mkdir .vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    mkdir .vim/colors
+    curl -o .vim/colors/256-grayvim.vim http://www.vim.org/scripts/download_script.php?src_id=12849
+    pacaur -S texworks
 
-mkdir .config/i3
-mkdir .config/termite
-mkdir .config/rofi
-mkdir .config/dunst
-mkdir .config/compton
-ln -s /home/tbrooks/projects/dotfiles/.bash_profile .bash_profile
-ln -s /home/tbrooks/projects/dotfiles/.tmux.conf .tmux.conf
-ln -s /home/tbrooks/projects/dotfiles/.config/i3/config .config/i3/config
-ln -s /home/tbrooks/projects/dotfiles/.config/termite/config .config/termite/config
-ln -s /home/tbrooks/projects/dotfiles/.config/polybar .config/polybar
-ln -s /home/tbrooks/projects/dotfiles/.config/rofi/config .config/rofi/config
-ln -s /home/tbrooks/projects/dotfiles/.config/dunst/config .config/dunst/config
-ln -s /home/tbrooks/projects/dotfiles/.config/compton/config .config/compton/config
+    mkdir .config/i3
+    mkdir .config/termite
+    mkdir .config/rofi
+    mkdir .config/dunst
+    mkdir .config/compton
+    ln -s /home/tbrooks/projects/dotfiles/.bash_profile .bash_profile
+    ln -s /home/tbrooks/projects/dotfiles/.tmux.conf .tmux.conf
+    ln -s /home/tbrooks/projects/dotfiles/.config/i3/config .config/i3/config
+    ln -s /home/tbrooks/projects/dotfiles/.config/termite/config .config/termite/config
+    ln -s /home/tbrooks/projects/dotfiles/.config/polybar .config/polybar
+    ln -s /home/tbrooks/projects/dotfiles/.config/rofi/config .config/rofi/config
+    ln -s /home/tbrooks/projects/dotfiles/.config/dunst/config .config/dunst/config
+    ln -s /home/tbrooks/projects/dotfiles/.config/compton/config .config/compton/config
 
-gpg --recv-keys 0FC3042E345AD05D
-pacaur -S discord
-pacaur -S spotify
+    gpg --recv-keys 0FC3042E345AD05D
+    pacaur -S discord
+    pacaur -S spotify
 
 EOF
 EOSH

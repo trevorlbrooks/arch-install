3dc63eaa34aade90777af5b8b82ac6582238609f
Trevor Brooks <tbrooks@mtech.edu>
2018-01-26 15:23:45 -0700

    Fix download url for sudo installs. Add wget for tempSudo file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pacstrap /mnt base
 genfstab -L /mnt >> /mnt/etc/fstab
-arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/trevorlbrooks.com/arch-install/master/sudoInstalls.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/trevorlbrooks/arch-install/master/sudoInstalls.sh)"
 echo "Setup complete. You should now set root and user passwords."
 
